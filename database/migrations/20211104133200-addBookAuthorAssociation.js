aeaadad74694ddfc84f2429e3a0907c7d26b0e7c
Dorian Buck <dorianbuck@gmail.com>
2021-11-05 11:21:08 +0100

    adds seeder for books and authors

--- database/migrations/20211104133200-addBookAuthorAssociation.js
+++ database/migrations/20211104133200-addBookAuthorAssociation.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.addcolumn("books", "AuthorId", {
+    return queryInterface.addColumn("Books", "AuthorId", {
       type: Sequelize.INTEGER,
       references: { model: "Authors", as: "author", key: "id" },
     });
